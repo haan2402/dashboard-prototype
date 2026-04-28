f53e6ade3cf001bed7f32046789d54b83369ca9e
Hanna Angeria <haan2402@student.miun.se>
2026-04-28 15:22:14 +0200

    skapat komponent förenskild tidsrapportering samt hämtat in den komponenten i montör-vyn

---
+++ src/components/widgets/TimeReport.vue
@@ -0,0 +1,47 @@
+<!--widget för tidrapportering - enskild rapportering-->
+<template>
+  <div class="time-report box">
+    <h3 class="widget-title title is-5 mb-4">Tidrapportering - denna vecka</h3>
+
+    <div class="time-summary mb-4">
+        <span class="reported-hours is-size-3 has-text-weight-bold">23h</span>
+        <span class="total-hours is-size-6 has-text-grey ml-2">av 40h</span>
+    </div>
+
+    <progress class="custom-progress progress" value="23" max="40">23/40</progress>
+
+    <a href="#" class="report-link has-text-weight-medium">Rapportera</a>
+  </div>
+</template>
+
+<script setup></script>
+
+<!--kompletterande styling-->
+<style scoped>
+    .widget-title {
+        color: #4A4A4A;
+    }
+
+    .reported-hours {
+        color: #595959;
+    }
+
+    .total-hours {
+        color: #595959;
+        font-weight: 500;
+    }
+
+    .custom-progress::-webkit-progress-value {
+        background-color: #FEAC31;
+    }
+
+    .custom-progress::-moz-progress-bar {
+        background-color: #FEAC31;
+    }
+
+    .report-link {
+        color: #39378E;
+        font-weight: 600;
+    }
+    
+</style>
--- src/views/MontorView.vue
+++ src/views/MontorView.vue
@@ -1,7 +1,16 @@
-<script setup></script>
+<script setup>
+import TimeReport from '@/components/widgets/TimeReport.vue';
+</script>
 
+<!--vy för montörer-->
 <template>
-  <h1>Montör</h1>
+  <div class="columns is-multiline">
+
+  <div class="column is-4">
+    <TimeReport />
+  </div>
+
+</div>
 </template>
 
 <style scoped></style>
