061214f9ec1fca5d22b0e1bff46cc4dfbc5f2f49
Hanna Angeria <haan2402@student.miun.se>
2026-04-29 15:10:36 +0200

    tagit bort en css fil som följde med, sedan har jag lagt till inter som typsnitt, samt justerat en rubrik

--- src/components/widgets/TimeReport.vue
+++ src/components/widgets/TimeReport.vue
@@ -1,11 +1,11 @@
 <!--widget för tidrapportering - enskild rapportering-->
 <template>
   <div class="time-report box">
-    <h3 class="widget-title title is-5 mb-4">Tidrapportering - denna vecka</h3>
+    <h3 class="widget-title is-5 mb-4 has-text-weight-semibold">Tidrapportering - denna vecka</h3>
 
     <div class="time-summary mb-4">
-        <span class="reported-hours is-size-3 has-text-weight-bold">23h</span>
-        <span class="total-hours is-size-6 has-text-grey ml-2">av 40h</span>
+        <span class="reported-hours is-size-3 has-text-weight-semibold">23h</span>
+        <span class="total-hours is-size-6 ml-2">av 40h</span>
     </div>
 
     <progress class="custom-progress progress" value="23" max="40">23/40</progress>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import 'bulma/css/bulma.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
+import './assets/base.css'
 
 const app = createApp(App)
 
