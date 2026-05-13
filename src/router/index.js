f4e28ee106c8e95502d81d27eb1b341089b46e61
Hanna Angeria <haan2402@student.miun.se>
2026-05-13 14:17:35 +0200

    tagit bort redigeringvy, samt tagit bort från routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AdminView from '@/views/AdminView.vue'
-import DashboardEditorView from '@/views/DashboardEditorView.vue'
 import MontorView from '@/views/MontorView.vue'
 
 const router = createRouter({
@@ -14,13 +13,8 @@ const router = createRouter({
     path: "/admin",
     name: "admin",
     component: AdminView
-  },
-  {
-    path: "/editor",
-    name: "editor",
-    component: DashboardEditorView
-  }
-],
-})
+  } 
+]
+});
 
 export default router
